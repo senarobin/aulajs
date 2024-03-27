fb8d6ba31e155afb6e95cebf305aa5d246da90e6
senarobin <140357675+senarobin@users.noreply.github.com>
2024-03-27 09:15:19 -0300

    Add files via upload

--- Exemplos/scripts/functions.js
+++ Exemplos/scripts/functions.js
@@ -1,6 +1,11 @@
-function digitarValor(valor) {
-    const valorDisplay = document.getElementById("display");
-    valorDisplay.value = valorDisplay.value === "0" ? valor : valorDisplay.value + valor;
+function digitarValor(valor){
+    valorDisplay = document.getElementById("display");
+
+    if(valorDisplay.value == 0){
+        valorDisplay.value = valor;
+    } else {
+        valorDisplay.value += valor;
+    }
 }
 
 function imprimirMsg(texto) {
@@ -34,19 +39,51 @@ function zerar() {
     document.getElementById("display").value = "0";
 }
 
-function login() {
+function digitarValor(valor){
+    valorDisplay = document.getElementById("display");
+
+    if(valorDisplay.value == 0){
+        valorDisplay.value = valor;
+    } else {
+        valorDisplay.value += valor;
+    }
+}
+
+
+function imprimirMsg(texto){
+    alert('Vc clickou no botão ' + texto);
+}
+
+function login(){
     const emailCorreto = "luciano@gmail.com";
     const senhaCorreta = "senha";
+    
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("pwd").value;
 
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("pwd").value;
+    if(email == emailCorreto){        
+        if(senha == senhaCorreta){
 
-    if (email === emailCorreto && senha === senhaCorreta) {
-        alert("E-mail e senha corretos");
-        window.location.href = "../index.html";
-    } else if (email === emailCorreto) {
-        alert("Senha incorreta.");
+            alert("E-mail e senha corretos");
+            window.location.href = "../index.html";
+
+        } else {
+            alert("Senha incorreta.");
+        }
+
     } else {
         alert("E-mail incorreto");
     }
 }
+
+    /*
+    if(email == emailCorreto && senha == senhaCorreta ){
+        alert("E-mail e senha corretos");
+    } else {
+        alert("E-mail ou senha incorretos.");
+    }
+
+    alert("E-mail digitado: " + email);
+    alert("Senha digitada: " + senha);
+*/
+
